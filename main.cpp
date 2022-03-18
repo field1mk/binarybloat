c864a47f19c9e48e14cac1f5b6e84f6130c50822
field1mk@cmich.edu <field1mk@cmich.edu>
2022-03-17 23:43:25 -0400

    -Initialized project setup (temp. hello world program).

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "What's your name?" << endl;
+
+    string input = "";
+
+    cin >> input;
+
+    cout << "Hi," << input << "!";
+
+    return 0;
+}
